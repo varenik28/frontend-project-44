b13c6cbe3c8459eac22bd13aaa397ad9e0e80ec1
Varvara Samoilova <varvarasamoilova2003300@gmail.com>
2025-12-02 14:27:57 +0400

    update progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,47 +5,28 @@ import getResult from '../result.js'
 
 const description = 'What number is missing in the progression?'
 
-const getProgression = () => {
+const getGameData = () => {
   const progression = []
+
   const start = getRandomIntInclusive()
-  const lastIndex = getRandomIntInclusive(5, 10)
+  const length = getRandomIntInclusive(5, 10)
   const step = getRandomIntInclusive(1, 10)
 
-  for (let i = 0; i < lastIndex; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     const currentElement = start + i * step
     progression.push(currentElement)
   }
 
-  const hideIndex = getRandomIntInclusive(0, lastIndex - 1)
+  const hideIndex = getRandomIntInclusive(0, length - 1)
+  const answer = String(progression[hideIndex])
   progression[hideIndex] = '..'
+  const question = progression.join(' ')
 
-  return progression.join(' ')
-}
-
-const findMissingNum = (progressionStr) => {
-  const progression = progressionStr.split(' ')
-  const numIndex = progression.indexOf('..')
-  let missingNum = 0
-  let step = 0
-
-  if (numIndex === 0) {
-    step += Number(progression[numIndex + 2]) - Number(progression[numIndex + 1])
-    missingNum += Number(progression[numIndex + 1]) - step
-  }
-  else if (numIndex === progression.length - 1) {
-    step += Number(progression[numIndex - 1]) - Number(progression[numIndex - 2])
-    missingNum += Number(progression[numIndex - 1]) + step
-  }
-  else {
-    step += (Number(progression[numIndex + 1]) - Number(progression[numIndex - 1])) / 2
-    missingNum += Number(progression[numIndex + 1]) - step
-  }
-
-  return String(missingNum)
+  return { question: question, answer: answer }
 }
 
 export const runBrainProgression = () => {
   const name = sayHello()
-  const result = runGame(description, getProgression, findMissingNum)
+  const result = runGame(description, getGameData)
   getResult(result, name)
 }
